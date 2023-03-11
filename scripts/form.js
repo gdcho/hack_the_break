90eb6418aa0ce590c6edd499d3b1c8b41b8d8003
David Cho <gcho13@my.bcit.ca>
2023-03-11 14:23:03 -0800

    form pre and next, authentication - david

--- scripts/form.js
+++ scripts/form.js
@@ -1,14 +1,12 @@
+var currentQuestionIndex = 0;
+var questionsPerPage = 5;
+var currentPageIndex = 0;
+
 function retrieveQuestions() {
     db.collection("questions").get().then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
         var question = doc.data().question;
-        var answers = [
-          doc.data().answer01,
-          doc.data().answer02,
-          doc.data().answer03,
-          doc.data().answer04,
-          doc.data().answer05,
-        ];
+        var answers = [          doc.data().answer01,          doc.data().answer02,          doc.data().answer03,          doc.data().answer04,          doc.data().answer05,        ];
   
         // Create radio button form for each question
         var questionForm = document.createElement("form");
@@ -18,31 +16,100 @@ function retrieveQuestions() {
           radioInput.type = "radio";
           radioInput.name = doc.id;
           radioInput.value = answers[i].points;
-          questionForm.appendChild(radioInput);
-          questionForm.innerHTML += "<label>" + answers[i].text + "</label><br/>";
+          radioInput.classList.add("form-check-input"); // Add the form-check-input class to the radio input
+  
+          var label = document.createElement("label");
+          label.innerHTML = answers[i].text;
+          label.classList.add("form-check-label"); // Add the form-check-label class to the label
+  
+          var formCheck = document.createElement("div");
+          formCheck.classList.add("form-check", "form-check-inline"); // Add the form-check and form-check-inline classes to the div container
+          formCheck.appendChild(radioInput);
+          formCheck.appendChild(label);
+          questionForm.appendChild(formCheck);
         }
+  
         document.getElementById("questions-form").appendChild(questionForm);
+        questionForm.classList.add("question");
       });
+      showQuestions(0);
+  
+      // Add Submit button
+      var questions = document.querySelectorAll(".question");
+      var numPages = Math.ceil(questions.length / questionsPerPage);
+      if (currentPageIndex === numPages - 1) {
+        var submitButton = document.createElement("button");
+        submitButton.innerHTML = "Submit";
+        submitButton.id = "submit-button";
+        submitButton.addEventListener("click", submitAnswers);
+        document.getElementById("questions-form").appendChild(submitButton);
+      }
     });
   }
   
-  // Submit answers
+  function showQuestions(startIndex) {
+    var questions = document.querySelectorAll(".question");
+    var endIndex = Math.min(startIndex + questionsPerPage, questions.length);
+    for (var i = 0; i < questions.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+        questions[i].style.display = "block";
+      } else {
+        questions[i].style.display = "none";
+      }
+    }
+    updateNavigation(startIndex, endIndex, questions.length);
+  }
+  
+  function updateNavigation(startIndex, endIndex, totalQuestions) {
+    var prevButton = document.getElementById("prev-button");
+    var nextButton = document.getElementById("next-button");
+    prevButton.disabled = startIndex == 0;
+    nextButton.disabled = endIndex == totalQuestions;
+    if (nextButton.disabled) {
+      nextButton.style.display = "none";
+    } else {
+      nextButton.style.display = "inline-block";
+    }
+  }
+  
+
+  function onNext() {
+    currentQuestionIndex += questionsPerPage;
+    currentPageIndex += 1;
+    showQuestions(currentQuestionIndex);
+  }
+  
+  function onPrev() {
+    currentQuestionIndex -= questionsPerPage;
+    currentPageIndex -= 1;
+    showQuestions(currentQuestionIndex);
+  }
+  
   function submitAnswers() {
     var answers = {};
     var forms = document.querySelectorAll("form");
-    for (var i = 0; i < forms.length; i++) {
-      var formInputs = forms[i].querySelectorAll("input");
-      for (var j = 0; j < formInputs.length; j++) {
-        if (formInputs[j].checked) {
-          answers[formInputs[j].name] = parseInt(formInputs[j].value);
-          break;
-        }
-      }
-    }
-  
-    // Do something with the answers (e.g. send to server, show results, etc.)
-    console.log(answers);
-  }
-  
-  // Call retrieveQuestions() on page load
-  window.onload = retrieveQuestions;
+
+  // Do something with the answers (e.g. send to server, show results, etc.)
+  console.log(answers);
+}
+
+// Call retrieveQuestions() on page load
+window.onload = retrieveQuestions;
+
+// Add navigation buttons
+var prevButton = document.getElementById("prev-button");
+var nextButton = document.getElementById("next-button");
+
+prevButton.addEventListener("click", function() {
+  onPrev();
+});
+
+nextButton.addEventListener("click", function() {
+  onNext();
+});
+
+
+var navigationContainer = document.getElementById("navigation-container");
+navigationContainer.appendChild(prevButton);
+navigationContainer.appendChild(nextButton);
+navigationContainer.appendChild(submitButton);
