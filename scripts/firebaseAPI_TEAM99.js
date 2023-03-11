fc2139a3f369b340c638622ee9867d6e579a781e
Noel kim <wkim51@my.bcit.ca>
2023-03-11 10:06:24 -0800

    added login function

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,7 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-
+    apiKey: "AIzaSyAmAINrFcDROBAFWq2steU5LQ6BXSc_YE0",
+        authDomain: "hackthebreak-77859.firebaseapp.com",
+        projectId: "hackthebreak-77859",
+        storageBucket: "hackthebreak-77859.appspot.com",
+        messagingSenderId: "1017671162794",
+        appId: "1:1017671162794:web:5572c3a2e171bd47d6a9d3"
 };
 
 //--------------------------------------------
@@ -11,4 +16,4 @@ var firebaseConfig = {
 //--------------------------------------------
 const app = firebase.initializeApp( firebaseConfig );
 const db = firebase.firestore();
-const storage = firebase.storage();
+
