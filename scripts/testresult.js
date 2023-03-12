16b041654a7a177da07aa1b3133c27006fe948ed
David Cho <gcho13@my.bcit.ca>
2023-03-11 18:22:42 -0800

    modal for testresult.html - david

--- scripts/testresult.js
+++ scripts/testresult.js
@@ -5,10 +5,11 @@ function displayScoreInfo() {
     if (user != null) {
       currentUser = db.collection("users").doc(user.uid);
       currentUser.get().then((userDoc) => {
-        var userscore = userDoc.data().totalscore;
-        // test use school data instead of scrore data
+        var userscore = userDoc.data().totalScore;
+        // test use school data instead of score data
         if (userscore != null) {
           document.getElementById("userscore").innerHTML = userscore;
+          console.log(userscore);
         }
       });
     } else {
@@ -18,28 +19,21 @@ function displayScoreInfo() {
 }
 displayScoreInfo();
 
-var modal = document.getElementById("save-modal");
-var backdrop = document.getElementById("backdrop");
-
-function openModal() {
-  //   modal.classList.add("show");
-  //   document.body.classList.add("modal-open");
-  backdrop.style.display = "block";
+// modal
+var modal = document.getElementById("myModal");
+// modal button
+var btn = document.getElementById("myBtn");
+// open modal on click
+btn.onclick = function() {
   modal.style.display = "block";
-  modal.classList.add("show");
 }
-function closeModal() {
-  //   modal.classList.remove("show");
-  //   document.body.classList.remove("modal-open");
-  backdrop.style.display = "none";
-  modal.style.display = "none";
-  modal.classList.remove("show");
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
+// close module click anywhere
+window.onclick = function(event) {
   if (event.target == modal) {
     closeModal();
-    window.location.href="main.html";
+    window.location.href = "main.html";
   }
-};
+}
+function closeModal() {
+  modal.style.display = "none";
+}
