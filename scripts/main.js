fb4584234879c6c6dbe6d35e5f2d4dc5092c6607
Noel kim <wkim51@my.bcit.ca>
2023-03-11 19:15:58 -0800

    added profile

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,17 @@
-// function readQuote( day ) {
-//     db.collection( "quotes" ).doc( day ).onSnapshot( tuesdayDoc => {
-//         console.log("inside")
-//         console.log( tuesdayDoc.data() )
-//         document.getElementById( "quote-goes-here" ).innerHTML = tuesdayDoc.data().quote;
-//     } )
-// }
-// readQuote( "tuesday" )
 
+function readQuote(day) {
+  db.collection("quotes").doc(day)                                                      //name of the collection and documents should matach excatly with what you have in Firestore
+    .onSnapshot(tuesdayDoc => {                                                               //arrow notation
+         console.log("current document data: " + tuesdayDoc.data());                          //.data() returns data object
+         document.getElementById("quote-goes-here").innerHTML = tuesdayDoc.data().quote;      //using javascript to display the data on the right place
+         
+         //Here are other ways to access key-value data fields
+         //$('#quote-goes-here').text(tuesdayDoc.data().quote);         //using jquery object dot notation
+         //$("#quote-goes-here").text(tuesdayDoc.data()["quote"]);      //using json object indexing
+         //document.querySelector("#quote-goes-here").innerHTML = tuesdayDoc.data().quote;
+    })
+}
+readQuote("tuesday");        //calling the function
 function insertNameFromFirestore() {
   //check if user is logged in
   firebase.auth().onAuthStateChanged((user) => {
