e1c739b2a32420018375cbdccef3add635129952
Noel kim <wkim51@my.bcit.ca>
2023-03-11 19:21:20 -0800

    fixed

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,50 +1,42 @@
-var currentUser;          //put this right after you start script tag before writing any functions.
+var currentUser;
 
 function populateUserInfo() {
     firebase.auth().onAuthStateChanged(user => {
-        // Check if user is signed in:
         if (user) {
-
-            //go to the correct user document by referencing to the user uid
             currentUser = db.collection("users").doc(user.uid)
-            //get the document for current user.
             currentUser.get()
                 .then(userDoc => {
-                    //get the data fields of the user
                     var userName = userDoc.data().name;
-                    var userAge = userDoc.data().Age;
-                    var userGender = userDoc.data().Gender;
+                    var userAge = userDoc.data().age; // age로 수정
+                    var userGender = userDoc.data().gender; // gender로 수정
 
-                    //if the data fields are not empty, then write them in to the form.
                     if (userName != "") {
                         document.getElementById("nameInput").value = userName;
                     }
                     if (userAge != "") {
-                        document.getElementById("ageInput").value = userAge
+                        document.getElementById("ageInput").value = userAge;
+                    }
                     if (userGender != "") {
                         document.getElementById("genderInput").value = userGender;
                     }
-           }   })
+                })
         } else {
-            // No user is signed in.
             console.log ("No user is signed in");
         }
     });
 }
 
-//call the function to run it 
 populateUserInfo();
+
 function editUserInfo() {
-    //Enable the form fields
     document.getElementById('personalInfoFields').disabled = false;
- }
- function saveUserInfo() {
-    //a) get user entered values
+}
+
+function saveUserInfo() {
     userName = document.getElementById('nameInput').value;
     userAge = document.getElementById('ageInput').value;
     userGender = document.getElementById('genderInput').value;
 
-    //b) update user's document in Firestore
     currentUser.update({
         name: userName,
         age: userAge,
@@ -57,6 +49,5 @@ function editUserInfo() {
         console.error("Error updating user information: ", error);
     });
 
-    //c) disable edit 
     document.getElementById('personalInfoFields').disabled = true;
 }
